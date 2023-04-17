c34dc89946619512d8fe78bf5675566041c5d64c
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-04-17 14:06:25 +0545

    feat: table created in DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,14 +12,15 @@ import { UserModule } from './user/user.module';
       // envFilePath: ".prod.env"
     })],
     useFactory: (configService: ConfigService) => ({
-      type: "postgres",
-      host: configService.get("DB_HOST"),
-      port: configService.get<number>("DB_PORT"),
-      username: configService.get("DB_USERNAME"),
-      password: configService.get("DB_PASSWORD"),
-      database: configService.get("DB_DATABASE"),
-      synchronize: configService.get<boolean>("DB_SYNC"),
-      entities: [__dirname + "/**/*.entity{.ts, .js}"]
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_DATABASE'),
+      synchronize: configService.get<boolean>('DB_SYNC'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true
     }),
     inject: [ConfigService]
   }), UserModule],
