e41ee5325b1a87f1ba178efbc045164084caa252
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-04-17 13:22:04 +0545

    feat: initial conf of typeorm done

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+@Module({
+  imports: [TypeOrmModule.forRootAsync({
+    imports: [ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".local.env",
+      // envFilePath: ".prod.env"
+    })],
+    useFactory: (configService: ConfigService) => ({
+      type: "postgres",
+      host: configService.get("DB_HOST"),
+      port: configService.get<number>("DB_PORT"),
+      username: configService.get("DB_USERNAME"),
+      password: configService.get("DB_PASSWORD"),
+      database: configService.get("DB_DATABASE"),
+      synchronize: configService.get<boolean>("DB_SYNC"),
+    }),
+    inject: [ConfigService]
+  })],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
